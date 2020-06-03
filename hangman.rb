f88c2a7a0908a56564e2131198e56a491f17ef27
frmazzoletti <franco_mazzoletti@hotmail.com>
2020-06-02 22:20:07 -0300

    Adds hangman and implements draw_hangman function

---
+++ hangman.rb
@@ -0,0 +1,29 @@
+=begin
+      _____
+     |     |
+     |     o
+     |    /|\
+   __|__  / \
+ _|_____|_
+|_________|
+=end
+
+def draw_hangman(n)
+
+  hangman = ["      _____", "     |     |", "     |", "     |", "   __|__", " _|_____|_", "|____#{n}____|"]
+
+  hangman[2] = "     |     o" if n > 0
+  hangman[3] = "     |     |" if n > 1
+  hangman[3] = "     |    /|" if n > 2
+  hangman[3] = "     |    /|\\" if n > 3
+  hangman[4] = "   __|__  /" if n > 4
+  hangman[4] = "   __|__  / \\" if n > 5
+
+  hangman
+end
+
+0.upto(6) { |i| puts draw_hangman(i)}
+
+
+
+
